b42bc52e1aee553f45d03e41e3b4834cd99792e5
Devin Hunt <devinhunt@gmail.com>
2012-05-01 16:01:56 +0100

    Ghosts main execution script created

---
+++ js/ghosts.js
@@ -0,0 +1,14 @@
+/**
+ * Pusher Ghosts
+ * A simple multiplayer gave that shows off the abilities of web-sockets and
+ * real time messaging
+ */
+
+(function() {
+    
+  var root = this,
+      PUSHER_APP_KEY = '22bc0fb0343194de2f30';
+      
+  
+  
+}).call(this);
